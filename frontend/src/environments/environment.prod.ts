af618cc8496858d15811314587d4245ae5b0e251
willummp <lsousaah@gmail.com>
2025-10-13 15:57:24 -0300

    fix: update production API URL in environment configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-// frontend/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  // Substitua pelo URL que você copiou da Railway!
-  apiUrl: 'https://URL-DA-RAILWAY.up.railway.app/api/v1'
+  apiUrl: '[https://rangerdashboardplatform-production.up.railway.app/api/v1](https://rangerdashboardplatform-production.up.railway.app/api/v1)'
 };
