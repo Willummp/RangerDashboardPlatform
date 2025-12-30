202a919dc99390ff45f8aa0ab408720dae6c1402
Willummp <lsousaah@gmail.com>
2025-12-29 21:19:30 -0300

    fix(front): update prod api url to real render instance

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
-  /*
-   * SUGESTÃO: Ao criar o Web Service no Render, ele te dará uma URL (ex: https://ranger-dashboard.onrender.com).
-   * Você deve colar ela aqui.
-   */
-  apiUrl: 'https://ranger-dashboard-api.onrender.com/api/v1',
-  wsUrl: 'wss://ranger-dashboard-api.onrender.com/api/v1'
+  apiUrl: 'https://rangerdashboardplatform.onrender.com/api/v1',
+  wsUrl: 'wss://rangerdashboardplatform.onrender.com/api/v1'
 };
